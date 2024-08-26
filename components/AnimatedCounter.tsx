2f2b9ea0d33d543d07aab154c74a5573f9b51a40
markwallace-git <123356752+markwallace-git@users.noreply.github.com>
2024-08-26 10:24:25 -0700

    Committing Banking app

---
+++ components/AnimatedCounter.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import CountUp from 'react-countup'
+
+function AnimatedCounter({amount}: {amount: number}) {
+  return (
+    <div className='w-full'>
+      <CountUp end={amount}
+      decimal='.'
+      decimals={2}
+      prefix='$'
+      duration={2}/>
+    </div>
+  )
+}
+
+export default AnimatedCounter
---
+++ components/MobileNav.tsx
@@ -0,0 +1,61 @@
+"use client"
+import {Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger,} from "@/components/ui/sheet"
+import { sidebarLinks } from "@/constants"
+import { cn } from "@/lib/utils"
+import Image from "next/image"
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+import Footer from "./Footer"
+
+const MobileNav = ( {user}: MobileNavProps) => {
+    const pathname= usePathname();
+  return (
+    <section className="w-full max-w-[264px]">
+        <Sheet>
+            <SheetTrigger>
+                <Image 
+                src="/icons/hamburger.svg"
+                width={30}
+                height={30}
+                alt="menu"
+                className="cursor-pointer"/>
+            </SheetTrigger>
+            <SheetContent  className=" border-none bg-white" side='left'>
+            <Link href='/'
+        className='mb-12 cursor-pointer items-center gap-2 flex'>
+            <Image 
+            src={"/icons/logo.svg"}
+            width={34}
+            height={34}
+            alt='Horizon-logo'
+            className='size-[24px] max-xl:size-14'/>
+            <h1 className='sidebar-logo'>Horizon</h1>
+        </Link>
+        {sidebarLinks.map((item)=>{
+            const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`)
+            return(
+                <Link href={item.route} key={item.label}
+                className={cn('sidebar-link', {'bg-bank-gradient': isActive})}>
+                    <div className='relative size-6'>
+                        <Image
+                        src={item.imgURL}
+                        alt= {item.label}
+                        fill
+                        className={cn({'brightness-[3] invert-0': isActive})}/>
+                        
+                    </div>
+                    <p className={cn('sidebar-label', {'!text-white' :isActive})}>{item.label}</p>
+        </Link>
+            )
+        }
+
+            )
+        };
+        <Footer user={user} type='mobile'/>
+            </SheetContent>
+        </Sheet>
+    </section>
+  )
+}
+
+export default MobileNav
